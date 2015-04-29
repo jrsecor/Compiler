5a5c7bf1502b96422e9712cb714fd54ad0b6361c
jrsecor <seecs2011@gmail.com>
2015-04-29 11:44:00 -0400

    Dr G's Code
    HERE

---
+++ src/Parser/CodeGenerationException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Parser;
+
+/**
+ *
+ * @author Seecs
+ */
+public class CodeGenerationException extends Exception {
+    public CodeGenerationException (String s){
+        super(s);
+    }
+}
